e8b420f76fa29297d8b3688bdb5f8e9eb5d64a66
Luke Martin <ljmart02@gmail.com>
2021-01-27 19:44:37 -0700

    Heroku

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workouts", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
   useCreateIndex: true,
-  useFindAndModify: false
+  useFindAndModify: true
 });
 
 require("./routes/api-routes.js")(app);
